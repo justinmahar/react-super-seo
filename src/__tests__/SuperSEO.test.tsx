8d0c7bed5616c9e5c39b8fdd9d0c4ab9f1738382
Dev Boldly <devboldly@gmail.com>
2020-05-02 15:56:24 -0500

    Update documentation

--- src/__tests__/SuperSEO.test.tsx
+++ src/__tests__/SuperSEO.test.tsx
@@ -2,9 +2,6 @@ import { render, RenderResult } from '@testing-library/react';
 import React from 'react';
 import { SuperSEO, SuperSEOProps } from '../components/SuperSEO';
 
-// Learn how to test React components:
-// https://testing-library.com/docs/react-testing-library/intro
-
 function renderComponent(props: SuperSEOProps, children: React.ReactNode = undefined): RenderResult {
   return render(<SuperSEO {...props}>{children}</SuperSEO>);
 }
